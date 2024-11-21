4b2ec4b0f83ff4a1583346e3596ece0b966421c6
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-11-21 22:50:16 +0300

    feat: Events detail page - add EventDetail page to display full details of an event.
    add new event feature

--- src/components/Events/EventsDetails.jsx
+++ src/components/Events/EventsDetails.jsx
@@ -1,10 +1,77 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet, useParams} from "react-router-dom";
+import Header from "../Header.jsx";
+import {useEventQuery} from "../../services/queries.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails(){
+    const params = useParams()
+
+    const {data, isPending, isError, error} = useEventQuery(params.id)
+
+    let content;
+
+    if (isPending) {
+        content = (
+            <div id="event-details-content" className="center">
+                Fetching event data ...
+            </div>
+        )
+    }
+
+    if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message || "failed to fetch event details"}
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button >Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
     return (
         <>
             <Outlet/>
-            <h1>Event Details</h1>
+            <Header>
+                <Link to="/events" className="nav-item">
+                    View All Events
+                </Link>
+            </Header>
+            <article id="event-details">
+                {content}
+            </article>
         </>
     )
 }
--- src/services/queries.js
+++ src/services/queries.js
@@ -1,8 +1,9 @@
 import {QueryClient, useQuery} from "@tanstack/react-query";
-import {fetchEvents} from "./http.js";
+import {fetchEvent, fetchEvents} from "./http.js";
 
 export const queryClient = new QueryClient();
 
+// fetch all events
 export function useEventsQuery(){
     return useQuery({
         queryKey: ["events", {max: 3}],
@@ -11,10 +12,19 @@ export function useEventsQuery(){
     })
 }
 
+// fetch an event based on a search term
 export function useSearchEventQuery(searchTerm) {
     return useQuery({
         queryKey:["events", {searchTerm: searchTerm}], //
         queryFn: ({signal, queryKey}) => fetchEvents({signal, ...queryKey[1]}), //signal:allows the request to be canceled if it’s still pending, to prevent race conditions or unnecessary network usage if the user changes the search term quickly.
         enabled: !!searchTerm, // Controls when this query is active by allowing fetching if there is a searchTerm.
+    })
+}
+
+// fetch an event of certain id for events pages
+export function useEventQuery(id){
+    return useQuery({
+        queryKey: ["events", {id}],
+        queryFn: ({signal}) => fetchEvent({signal, id:id}),
     })
 }
